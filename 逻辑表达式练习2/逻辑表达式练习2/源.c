ecbd102cd93565cb8eba806137dcb7b0e5d6e6a0
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-04-03 13:55:46 +0800

    Add files via upload
    逻辑表达式练习2

---
+++ 逻辑表达式练习2/逻辑表达式练习2/源.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++ && ++b && d++;
+	printf("a=%d b=%d c=%d d=%d\n", a, b, c, d);
+	i = a++ || ++b || d++;
+	printf("a=%d b=%d c=%d d=%d\n", a, b, c, d);
+	return 0;
+}
