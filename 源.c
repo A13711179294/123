08f90b0f98e47533cb765c96c37cfdfffed400e4
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-05-04 19:57:43 +0800

    Add files via upload
    55-100

---
+++ 源.c
@@ -0,0 +1,12 @@
+//将一个数组逆序输出
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (sz = sz - 1; sz >= 0; sz--)
+	{
+		printf("%d ", arr[sz]);
+	}
+	return 0;
+}
