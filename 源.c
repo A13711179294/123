2cd860ac2855293efb7e11886c0592bb0703fe51
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-05-17 18:18:32 +0800

    Add files via upload
    指针进阶2

---
+++ 源.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void print(int* ptr, int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(ptr + i));
+	}
+}
+void test(char* p)
+{
+	printf("%c ", *p);
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(p, sz);
+	char ch = 'w';
+	char* p1 = &ch;
+	test(&ch);
+	test(p1);
+	return 0;
+}
