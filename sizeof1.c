0b6c76d5fcac45fb560cfe647a2d58a25c28e0e5
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-05-20 12:58:32 +0800

    Rename 源.c to sizeof1.c

--- sizeof1.c
+++ sizeof1.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	//��������
+	//整型数组
 	int a[] = { 1,2,3,4 };
 	printf("%d\n", sizeof(a));//16
-	printf("%d\n", sizeof(a+0));//4/8 a+0�ǵ�һ��Ԫ�صĵ�ַ��sizeof(a+0)������ǵ�ַ��С
-	printf("%d\n", sizeof(*a));//4 *a������ĵ�һ��Ԫ�أ�sizeof(*a)������ǵ�һ��Ԫ�صĴ�С
-	printf("%d\n", sizeof(a+1));//4/8 a+1�ǵڶ���Ԫ�ص�ַ��sizeof(a+1)������ǵ�ַ�Ĵ�С
-	printf("%d\n", sizeof(a[1]));//4 ������ǵڶ���Ԫ�صĴ�С
+	printf("%d\n", sizeof(a+0));//4/8 a+0是第一个元素的地址，sizeof(a+0)计算的是地址大小
+	printf("%d\n", sizeof(*a));//4 *a是数组的第一个元素，sizeof(*a)计算的是第一个元素的大小
+	printf("%d\n", sizeof(a+1));//4/8 a+1是第二个元素地址，sizeof(a+1)计算的是地址的大小
+	printf("%d\n", sizeof(a[1]));//4 计算的是第二个元素的大小
 
-	printf("%d\n", sizeof(&a));//4/8 ������ǵ�ַ�Ĵ�С
-	printf("%d\n", sizeof(*&a));//16 ��������Ĵ�С
+	printf("%d\n", sizeof(&a));//4/8 计算的是地址的大小
+	printf("%d\n", sizeof(*&a));//16 计算数组的大小
 	//int(*p)[4]=&a
-	printf("%d\n", sizeof(&a + 1));//4/8 �������Ŀռ�ĵ�ַ
+	printf("%d\n", sizeof(&a + 1));//4/8 数组后面的空间的地址
 	printf("%d\n", sizeof(&a[0]));//4/8
 	printf("%d\n", sizeof(&a[0] + 1));//4/8
 
-	//�ַ�����
+	//字符数组
 	char arr[] = { 'a','b','c','d','e','f' };
 	printf("%d\n", sizeof(arr));//6
 	printf("%d\n", sizeof(arr + 0));//4/8
