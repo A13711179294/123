92783751e410fc220dd73e2c9517c53278548e18
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-06-18 11:57:46 +0800

    Add files via upload
    6-4.c

---
+++ 源.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int x = 0, i = 0, a = 0;
+	int arr[8] = { 5,21,32,51,76,98,762 };
+	printf("请输入一个数字\n");
+	scanf_s("%d", &x);
+	for (i = 0; i < 8; i++)
+	{
+		if (arr[i] < x && x < arr[i + 1])
+		{
+			a = arr[i + 1];
+			arr[i + 1] = x;
+			x = a;
+		}
+		else
+			arr[7] = x;
+	}
+	for (i = 0; i < 8; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
