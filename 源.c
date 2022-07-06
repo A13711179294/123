2bd2a39ed400f951f0d381e410efb13f380fa07a
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-07-06 18:31:24 +0800

    Add files via upload
    8-7.c

---
+++ 源.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void cut_str(char* str1, char* str2, int M)
+{
+	int i = 0;
+	while (*(str1 + M - 1) != '\0')
+	{
+		*(str2 + i) = *(str1 + M - 1);
+		M++;
+		i++;
+	}
+}
+int main()
+{
+	int m = 0;
+	char ch1[81] = { 0 };
+	char ch2[81] = { 0 };
+	gets(ch1);
+	printf("«Î ‰»Îm\n");
+	scanf("%d", &m);
+	if (strlen(ch1) >= m)
+	{
+		cut_str(ch1, ch2, m);
+	}
+	else
+		printf(" ‰»Î¥ÌŒÛ\n");
+	puts(ch2);
+	return 0;
+}
